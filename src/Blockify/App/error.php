1ef60856313a84e10a6b05b326345f32d5bbcd6c
king-m-a-kh-85 <king.m.a.kh.85@gmail.com>
2022-05-29 21:26:29 +0430

    update error.php

--- src/Blockify/App/error.php
+++ src/Blockify/App/error.php
@@ -11,26 +11,30 @@ class error implements errorApplication
 
     public static function classError(string $className): Api
     {
+        header('Content-Type: application/json; charset=utf-8');
         return new Api(false, "$className class notFound");
     }
 
     public static function functionError(string $functionName): Api
     {
+        header('Content-Type: application/json; charset=utf-8');
         return new Api(false, "$functionName notFound");
     }
 
     public static function parameterError(array $parameterName): Api
     {
+        header('Content-Type: application/json; charset=utf-8');
         return new Api(false, "please enter parameters", $parameterName);
     }
 
     public static function parameterCountError(int $count): Api
     {
+        header('Content-Type: application/json; charset=utf-8');
         return new Api(false, "parameter count is $count");
     }
 
     public static function viewError(string $name): string
     {
-        return "error";
+        return "page $name not found";
     }
 }
